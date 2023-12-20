7483a10b1b41e78d49170ac115bc7777132d586f
bhimprasadrajbanshi <rajbanshibhimprasad123@gmail.com>
2023-12-20 21:23:21 +0545

    initial

---
+++ news_project/news_project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-10nm*3xdb16fme&!m2weo*o!kv76d00+-9o$i&ev%y=!rid&jd"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
